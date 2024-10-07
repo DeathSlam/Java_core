3731c7dfa93f62d9470ed28af3d5bf3801472273
Sergey <andr126dr@gmail.com>
2024-10-07 09:33:28 +0500

    Update

--- src/timus/task_1293.java
+++ src/timus/task_1293.java
@@ -1,4 +1,26 @@
 package timus;
 
+import java.util.Scanner;
+
 public class task_1293 {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите количество панелей");
+        int amountPlates = in.nextInt();
+
+        System.out.println("Введите ширину панелей");
+        int widthPlate = in.nextInt();
+
+        System.out.println("Введите длину панелей");
+        int lenghtPlate = in.nextInt();
+
+        int nanoSylf = amountPlates * (widthPlate * lenghtPlate) * 2;
+
+        System.out.println("Требуется " + nanoSylf + " нанограмм сульфида");
+
+
+    }
+
 }
