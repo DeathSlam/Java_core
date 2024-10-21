5739488251b7e332c8a3fc59061a541fc601e62b
Sergey <andr126dr@gmail.com>
2024-10-21 15:14:11 +0500

    Update

--- src/lr1/Example10.java
+++ src/lr1/Example10.java
@@ -2,9 +2,7 @@ package lr1;
 
 import java.time.Year;
 import java.util.Scanner;
-
 public class Example10 {
-
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
 
