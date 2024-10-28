93882346015cd91298ca63b9709b1d3b57f4bc58
Sergey <andr126dr@gmail.com>
2024-10-28 14:08:09 +0500

    update

---
+++ src/lr3/Example6.java
@@ -0,0 +1,18 @@
+package lr3;
+
+import java.util.Scanner;
+
+public class Example6 {
+    public static void main(String[] args)
+    {
+        //TODO Finish
+        Scanner in = new Scanner(System.in);
+
+        int arrayNum = in.nextInt();
+        if (arrayNum < 0) System.out.println("Введено не верное количество ячеек массива");
+        int[] array = new int[arrayNum];
+        int foundNum = 0;
+
+
+    }
+}
